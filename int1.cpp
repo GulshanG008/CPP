570e05be590b464971fb198c318359f1d10980f0
Gulshan Godsora <gulshangodsora008@gmail.com>
2024-10-31 16:10:21 +0530

    C++ programs

---
+++ int1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 3;
+    cout << "The number is: " << x <<endl;
+    int y;
+    cout << "Enter an integer: " <<endl;
+    cin >> y;
+    cout << "The  enteredd integer is:" <<y << endl;
+    return 0;
+}
---
+++ sum1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=3,b=4;
+    cout << "The sum of " << a << " and " << b << " is " << a+b <<endl;
+    int c,d;
+    cout << "Enter two numbers:";
+    cin >> c >> d;
+    cout << "The sum of " << c << " and " << d << " is " << c+d << endl; 
+    return 0;
+}
